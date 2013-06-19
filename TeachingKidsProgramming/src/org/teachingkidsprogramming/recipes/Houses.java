f9aee5afb2b8e9d9595b6e0528b9abae998dda5c
Jason Michael Huynh <j_huynh98@yahoo.com>
2013-06-19 15:48:06 -0700

    Roofs-Jason.Michael

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
@@ -8,7 +8,6 @@ public class Houses
   public static void main(String[] args)
   {
     Tortoise.setSpeed(10);
-    //   Have the tortoise start at 200 pixels in on the X axis --#14
     Tortoise.setX(200);
     int height = 40;
     drawHouse(height);
@@ -18,15 +17,46 @@ public class Houses
   }
   private static void drawHouse(int height)
   {
-    //   Change the color of the line the tortoise draws to lightGray --#15
     Tortoise.setPenColor(Colors.Grays.LightGray);
     Tortoise.move(height);
+    // flatRoof();
+    //amittaisRoof();
+    //MavericksRoof
+    //turn left 90
+    Tortoise.turn(-90);
+    Tortoise.move(10);
+    Tortoise.turn(90 + 45);
+    Tortoise.move(50);
     Tortoise.turn(90);
-    Tortoise.move(30);
-    Tortoise.turn(90);
+    Tortoise.move(50);
+    Tortoise.turn(90 + 45);
+    Tortoise.move(10);
+    Tortoise.turn(-90);
+    //move 10
+    // turn right 90+45 degrees
+    // move 50
+    // turn 90
+    // move 50
+    // turn 90 + 45
+    // move 10
+    //turn 90 left
     Tortoise.move(height);
     Tortoise.turn(-90);
     Tortoise.move(20);
     Tortoise.turn(-90);
+  }
+  private static void amittaisRoof()
+  {
+    Tortoise.turn(45);
+    Tortoise.move(10);
+    Tortoise.turn(90);
+    Tortoise.move(10);
+    Tortoise.turn(45);
+  }
+  private static void flatRoof()
+  {
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
   }
 }
